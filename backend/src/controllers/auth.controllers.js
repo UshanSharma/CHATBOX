14020cb104626d4a5ee0a5e896da631914898885
ushan sharma <ushansharma9211@gmail.com>
2025-10-28 17:12:08 +0530

    login & logout functionality added

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -75,3 +75,30 @@ export const deleteAllUsers = async (req, res) => {
   }
 };
 
+export const login = async (req,res) =>{
+    const {email,password}= req.body
+    try {
+        const user= await User.findOne({email})
+        if (!user) return res.status(400).json ({message:"Invalid Credentials"})
+        // never tell the user which one is incorrect: email or password
+        const ispasswordCorrect= await bcrypt.compare(password, user.password)
+        if (!ispasswordCorrect) return res.status(400).json ({message:"Invalid Credentials"});
+
+        generateToken(user._id,res)
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic:user.profilePic,
+        });
+    } catch (error) {
+        console.error("Error in login controller:", error)
+        res.status(500).json({message: "Internal server error"})
+    }
+};
+
+export const logout = (_,res) => {
+    res.cookie ("jwt", "", {maxAge:0})
+    res.status(200).json({message:"Logged out successfully"})
+};
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,11 @@
 import express from "express";
-import { signup, deleteAllUsers } from "../controllers/auth.controllers.js";
+import { signup, login, logout, deleteAllUsers } from "../controllers/auth.controllers.js";
 
 const router= express.Router();
 
 router.post("/signup",signup);
 router.delete("/delete-all", deleteAllUsers);
-
-router.get("/login", (req,res) => {
-    res.send("Login endpoint");
-})
-
-router.get("/logout", (req,res) => {
-    res.send("Logout endpoint");
-})
+router.post("/login", login)
+router.post("/logout", logout)
 
 export default router;
