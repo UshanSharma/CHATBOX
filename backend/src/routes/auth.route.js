75d91a5c2b0ed7e29eb819b082c10787af706442
ushan sharma <ushansharma9211@gmail.com>
2025-10-28 16:16:41 +0530

    welcome email implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signup } from "../controllers/auth.controllers.js";
+import { signup, deleteAllUsers } from "../controllers/auth.controllers.js";
 
 const router= express.Router();
 
 router.post("/signup",signup);
+router.delete("/delete-all", deleteAllUsers);
 
 router.get("/login", (req,res) => {
     res.send("Login endpoint");
