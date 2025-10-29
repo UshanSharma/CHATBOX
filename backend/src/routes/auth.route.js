a4fe2cc38ba5b8f4f6b5819b1b9f70570d04ee27
ushan sharma <ushansharma9211@gmail.com>
2025-10-29 16:41:24 +0530

    update profile endpoint added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { signup, login, logout, deleteAllUsers } from "../controllers/auth.controllers.js";
+import { signup, login, logout, updateProfile,deleteAllUsers } from "../controllers/auth.controllers.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router= express.Router();
 
 router.post("/signup",signup);
 router.delete("/delete-all", deleteAllUsers);
-router.post("/login", login)
-router.post("/logout", logout)
+router.post("/login", login);
+router.post("/logout", logout);
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, (req,res) => res.status(200).json(req.user))
 
 export default router;
