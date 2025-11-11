2982db1044bba1e889cc75cdab9ea3dd1cbf0730
ushan sharma <ushansharma9211@gmail.com>
2025-11-11 19:23:03 +0530

    realtime time messages implemented

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -7,13 +7,17 @@ import MessageInput from './MessageInput';
 import MessagesLoadingSkeleton from './MessagesLoadingSkeleton';
 
 function ChatContainer() {
-  const {selectedUser,getMessagesByUserId, messages, isMessagesLoading}= useChatStore();
+  const {selectedUser,getMessagesByUserId, messages, isMessagesLoading, subscribeToMessages,unsubscribeFromMessages}= useChatStore();
   const {authUser} = useAuthStore();
   const messageEndRef= useRef(null)
 
   useEffect(() => {
-    getMessagesByUserId(selectedUser._id)
-  },[selectedUser,getMessagesByUserId])
+    getMessagesByUserId(selectedUser._id);
+    subscribeToMessages()
+
+    //clean up
+    return () => unsubscribeFromMessages()
+  },[selectedUser,getMessagesByUserId, subscribeToMessages, unsubscribeFromMessages])
 
   useEffect(() => {
     if (messageEndRef.current) {
